4197bc7b56bb7f9eed6dc26002399d0c97112f42
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-03-05 13:20:55 -0800

    Added star us on github button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import Image from "next/image"
-import { FaGithub } from "react-icons/fa"
 
 const Navbar = () => {
   return (
@@ -14,18 +13,37 @@ const Navbar = () => {
                 PlatoIO
               </p>
             </div>
-            <div className="flex flex-row items-center justify-center space-x-4">
-              <a
-                href="https://github.com/hqasmei/platoio"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaGithub
-                  size={25}
-                  className="text-neutral-800 hover:text-neutral-500"
-                />
-              </a>
-            </div>
+
+            <a
+              href="https://github.com/hqasmei/platoio"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="relative group">
+                <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-full "></div>
+                <div className="absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-full group-hover:blur group-hover:opacity-75 group-hover:transition group-hover:duration-200"></div>
+                <button className="relative px-4 py-2 bg-black rounded-full leading-none flex items-center">
+                  <span className="flex items-center space-x-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="white"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                      />
+                    </svg>
+
+                    <span className="text-gray-100">Star us on Github</span>
+                  </span>
+                </button>
+              </div>
+            </a>
           </div>
         </div>
       </div>
